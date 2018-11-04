d3f2d54d80cf976bf8b234402f171b8f3f326943
sjhaveri1 <shajhaveri@gmail.com>
2018-11-04 12:11:29 -0500

    fixed MST and added cost function calc

--- tree1.m
+++ tree1.m
@@ -8,9 +8,14 @@ run TREES1.15/start_trees
 
 node = 2;
 
-coords1 = coords/10;
+coords1 = coords/1;
 %img1 = imresize(img);
-for i=node:node
+%figure
+%imshow(img), hold on
+
+tic
+
+for i=1:122
     j=1;
     while cxns(j,1) ~= i
         j = j+1;
@@ -21,28 +26,35 @@ for i=node:node
         targets(k,:) = coords1(cxns(j,2),:);
         k = k+1;
         j = j+1;
+        if j > length(cxns)
+            break
+        end
     end
     X = [coords1(i,1);targets(:,1)];
     Y = [coords1(i,2);targets(:,2)];
     Z = [coords1(i,3);targets(:,3)];
     %Z = zeros(length(X),1);
-    figure
-    MST_tree (1, X,Y,Z, 0, 50,[],[],'-s');
+    %figure
+    MST_tree (1, X,Y,Z, 0, 900,[],[]);
+    %bCoords = [tree{i}.X, tree{i}.Y, tree{i}.Z];
     %MST_tree (1, X,Y,Z,0);
+    rows = size(targets);
+    rows = rows(1);
+    
+    for x=1:rows
+        %plot3(targets(x,1),targets(x,2),targets(x,3),'r.', 'MarkerSize',20), hold on
+        %plot3([main(1,1),targets(x,1)],[main(1,2),targets(x,2)],[main(1,3),targets(x,3)]), hold on
+        %plot(targets(x,1),targets(x,2),'r.', 'MarkerSize',20), hold on
+        %plot([targets(1,1),targets(x,1)],[targets(1,2),targets(x,2)]), hold on
+    end
+    
+    %figure
+    %HP = plot_tree(i,[],[],[],[],'-3l');
+    %hold on
+    %set(HP, 'marker','*'), hold on
+    i
 end
 
-rows = size(targets);
-rows = rows(1);
-figure
-%imshow(img), hold on
-for x=1:rows
-    plot3(targets(x,1),targets(x,2),targets(x,3),'r.', 'MarkerSize',20), hold on
-    plot3([main(1,1),targets(x,1)],[main(1,2),targets(x,2)],[main(1,3),targets(x,3)]), hold on
-    %plot(targets(x,1),targets(x,2),'r.', 'MarkerSize',20), hold on
-    %plot([targets(1,1),targets(x,1)],[targets(1,2),targets(x,2)]), hold on
-end
+toc
 
-%figure
-HP = plot_tree(1,[],[],[],[],'-3l');
-set(HP, 'marker','*')
-
+save('bMST.mtr','trees')
